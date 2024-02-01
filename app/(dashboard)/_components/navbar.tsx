18723b500672675b52740244daf6978ea8820222
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-01-31 21:25:43 -0600

    added search bar specs and params, created invites with button comp through clerk

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,41 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton, useOrganization } from "@clerk/nextjs";
 import React from "react";
+import { SearchInput } from "./search-input";
+import { InviteButton } from "./invite-button";
 
 export const Navbar = () => {
+  const { organization } = useOrganization();
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex-1 lg:flex bg-green-900">Search</div>
+    <div className="flex items-center gap-x-4 p-5 ">
+      <div className="hidden lg:flex-1 lg:flex ">
+        <SearchInput />{" "}
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "367px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                justifyContent: "space-between",
+                border: "1px solid #e5e7eb",
+              },
+            },
+          }}
+        />
+      </div>
+      {organization && <InviteButton />}
+
       <UserButton />
     </div>
   );
