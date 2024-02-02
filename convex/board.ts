0bdc09c690a1bb46b3901e4e4d1f5005cc575ea8
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-02-01 20:49:25 -0600

    set up grid for Team notes, added create board button to team notes, styled board cards

--- convex/board.ts
+++ convex/board.ts
@@ -3,16 +3,16 @@ import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
 const images = [
-  "/placeholders/ph1",
-  "/placeholders/ph2",
-  "/placeholders/ph3",
-  "/placeholders/ph4",
-  "/placeholders/ph5",
-  "/placeholders/ph6",
-  "/placeholders/ph7",
-  "/placeholders/ph8",
-  "/placeholders/ph9",
-  "/placeholders/ph10",
+  "/placeholders/ph1.png",
+  "/placeholders/ph2.png",
+  "/placeholders/ph3.png",
+  "/placeholders/ph4.png",
+  "/placeholders/ph5.png",
+  "/placeholders/ph6.png",
+  "/placeholders/ph7.png",
+  "/placeholders/ph8.png",
+  "/placeholders/ph9.png",
+  "/placeholders/ph10.png",
 ];
 
 export const create = mutation({
