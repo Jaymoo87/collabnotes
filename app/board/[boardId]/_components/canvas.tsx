1d595aa64c35ddf0b5a903d16e5f3eb8304cf038
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-02-07 21:34:08 -0600

    set up pointer events for rectangle element, defined layer types, LiveObject for rectangle layer placement.

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -2,30 +2,89 @@
 
 import React, { useCallback, useState } from "react";
 
-import { CanvasState, CanvasMode, Camera } from "@/types/canvas";
+import { nanoid } from "nanoid";
 
-import { useHistory, useCanRedo, useCanUndo, useMutation } from "@/liveblocks.config";
+import { CanvasState, CanvasMode, Camera, Color, LayerType, Point } from "@/types/canvas";
+
+import { useHistory, useCanRedo, useCanUndo, useMutation, useStorage } from "@/liveblocks.config";
 
 import { Info } from "./info";
 import { Toolbar } from "./toolbar";
 import { Participants } from "./participants";
 import { CursorsPresence } from "./cursors-presence";
 import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { LiveObject } from "@liveblocks/client";
+import { LayerPreview } from "./layer-preview";
+
+const MAX_LAYERS = 100;
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const layerIds = useStorage((root) => root.layerIds);
+
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
 
   const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+  const [lastUsedColor, setLastUsedColor] = useState<Color>({
+    r: 0,
+    b: 0,
+    g: 0,
+  });
 
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const insertLayer = useMutation(
+    (
+      { storage, setMyPresence },
+      layerType: LayerType.Circle | LayerType.Rectangle | LayerType.Note | LayerType.Text,
+      position: Point
+    ) => {
+      const liveLayers = storage.get("layers");
+
+      if (liveLayers.size >= MAX_LAYERS) {
+        return;
+      }
+      const liveLayerIds = storage.get("layerIds");
+      const layerId = nanoid();
+
+      const layer = new LiveObject({
+        type: layerType,
+        x: position.x,
+        y: position.y,
+        height: 100,
+        width: 100,
+        fill: lastUsedColor,
+      });
+
+      liveLayerIds.push(layerId);
+      liveLayers.set(layerId, layer);
+
+      setMyPresence({ selection: [layerId] }, { addToHistory: true });
+      setCanvasState({ mode: CanvasMode.None });
+    },
+    [lastUsedColor]
+  );
+
+  const onPointerUp = useMutation(
+    ({}, e) => {
+      const point = pointerEventToCanvasPoint(e, camera);
+
+      if (canvasState.mode === CanvasMode.Inserting) {
+        insertLayer(canvasState.layerType, point);
+      } else {
+        setCanvasState({ mode: CanvasMode.None });
+      }
+      history.resume();
+    },
+    [camera, canvasState, history, insertLayer]
+  );
 
   const onWheel = useCallback((e: React.WheelEvent) => {
     setCamera((camera) => ({
@@ -63,8 +122,12 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         onWheel={onWheel}
         onPointerMove={onPointerMove}
         onPointerLeave={onPointerLeave}
+        onPointerUp={onPointerUp}
       >
-        <g>
+        <g style={{ transform: `translate(${camera.x}px, ${camera.y}px)` }}>
+          {layerIds.map((layerId) => (
+            <LayerPreview key={layerId} id={layerId} onLayerPointerDown={() => {}} selectionColor={"#000"} />
+          ))}
           <CursorsPresence />
         </g>
       </svg>
---
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useStorage } from "@/liveblocks.config";
+import { LayerType } from "@/types/canvas";
+import React, { memo } from "react";
+import { Rectangle } from "./rectangle";
+
+interface LayerPreviewProps {
+  onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
+  selectionColor?: string;
+  id: string;
+}
+
+export const LayerPreview = memo(({ id, onLayerPointerDown, selectionColor }: LayerPreviewProps) => {
+  const layer = useStorage((root) => root.layers.get(id));
+
+  if (!layer) return null;
+
+  switch (layer.type) {
+    case LayerType.Rectangle:
+      return (
+        <Rectangle id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor as string} />
+      );
+    default:
+      console.warn("Unknown layer type");
+      return null;
+  }
+
+  return <div>LayerPreview</div>;
+});
+
+LayerPreview.displayName = "LayerPreview";
