e7da6bcfd88ebc93e635da6b969775480b99ec0d
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-02-17 20:30:58 -0600

    Circle, Text, and Sticky Note layers added, contrastcolors function, foreignObject practice

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -1,13 +1,18 @@
 "use client";
+import React, { memo } from "react";
 
 import { useStorage } from "@/liveblocks.config";
+
 import { LayerType } from "@/types/canvas";
-import React, { memo } from "react";
+
 import { Rectangle } from "./rectangle";
+import { Circle } from "./circle";
+import { Text } from "./text";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
   onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
-  selectionColor: string;
+  selectionColor?: string;
   id: string;
 }
 
@@ -17,14 +22,22 @@ export const LayerPreview = memo(({ id, onLayerPointerDown, selectionColor }: La
   if (!layer) return null;
 
   switch (layer.type) {
+    case LayerType.Circle:
+      return <Circle id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
     case LayerType.Rectangle:
       return <Rectangle id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
+    case LayerType.Text:
+      return <Text id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
+    case LayerType.Note:
+      return <Note id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
 
     default:
       console.warn("Unknown layer type");
       return null;
   }
-
   return <div>LayerPreview</div>;
 });
 
--- app/board/[boardId]/_components/rectangle.tsx
+++ app/board/[boardId]/_components/rectangle.tsx
@@ -4,18 +4,18 @@ import { RectangleLayer } from "@/types/canvas";
 interface RectangleProps {
   id: string;
   layer: RectangleLayer;
+  selectionColor?: string;
   onPointerDown: (e: React.PointerEvent, id: string) => void;
-  selectionColor: string;
 }
 
 import React from "react";
 
-export const Rectangle = ({ id, layer, onPointerDown, selectionColor }: RectangleProps) => {
+export const Rectangle = ({ id, layer, selectionColor, onPointerDown }: RectangleProps) => {
   const { x, y, height, width, fill } = layer;
 
   return (
     <rect
-      className="dropshadow-md "
+      className="drop-shadow-md "
       onPointerDown={(e) => onPointerDown(e, id)}
       style={{ transform: `translate(${x}px, ${y}px)` }}
       x={0}
